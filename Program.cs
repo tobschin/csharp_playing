37bc9a33d9b02997883abe7f066fb377b89d6ec0
tobiasschinagl <tobias.schinagl@codecentric.de>
2022-12-13 17:06:20 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            if (args.Length > 0)
+            {
+                Console.WriteLine(args[0]);
+            }
+
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# My First C# - App
+
+Tutorial: https://learn.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code?pivots=dotnet-7-0
+
+## Creation:
+```sh
+dotnet new <programmname>
+```
+
+## Execution
+```sh
+dotnet run
+```
